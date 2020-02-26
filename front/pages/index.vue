e128f59e977fd46071630bc8ba92c7c3968d8609
YOJI4910 <fmn.outlook@outlook.com>
2020-02-26 22:44:54 +0900

    [add]back->models, front->vuetify,firebase

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,72 +1,36 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxtenki
-      </h1>
-      <h2 class="subtitle">
-        tenki for onetap
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <AddTodo @submit="addTodo" />
+    <TodoList :todos="todos" />
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import AddTodo from "@/components/AddTodo";
+import TodoList from "@/components/TodoList";
 
 export default {
   components: {
-    Logo
+    AddTodo,
+    TodoList
+  },
+  created() {
+    console.log("API_KEY:", process.env.API_KEY);
+  },
+  data(){
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    addTodo(title) {
+      this.todos.push({
+        title
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
